9454c6f8a82182c2d67601b37d88452ac60abfe5
Gabby Manzano <gabbymanzano98@gmail.com>
2020-10-16 21:01:47 -0500

    added db folder

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno cheddar Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mexican Burger', true);
