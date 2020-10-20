59d8133241a1e6615d6a635ca6eb7b9c6405b243
Gabby Manzano <gabbymanzano98@gmail.com>
2020-10-20 18:46:57 -0500

    added controllers

--- config/orm.js
+++ config/orm.js
@@ -1,28 +1,95 @@
-var connection = require("../config/connection.js");
+var connection = require("../config/connection");
+
+
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+     
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = `'${value}'`;
+            }
+            arr.push(`${key}=${value}`);
+        }
+    }
+
+    
+    return arr.toString();
+}
+
 
 var orm = {
-    selectAll: function(tableInput, cb) {
-        let queryString = "SELECT * FROM " + tableInput + ";";
-
-        connection.query(queryString, function(err, res) {
-            if (err) throw err;
-            cb(res)
+    all: function(tableInput, cb) {
+        var queryString = `SELECT * FROM ${tableInput};`;
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
         });
     },
-    insertOne: function(newBurger, cb) {
-        let queryString = "INSERT INTO burger (burger_name) VALUES (" + newBurger + ");";
+    MakeNew: function(table, cols, vals, cb) {
+        var queryString = `INSERT INTO ${table}`;
 
-        connection.query(queryString, function(err, res) {
-            if (err) throw err;
-            cb(res);
-        })
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
     },
-    updateOne: function(burgerId, cb) {
-        let queryString = "UPDATE burgers SET devoured = true WHERE id = " + burgerId;
-        connection.query(queryString, function(err, res) {
-            if (err) throw err;
-            cb(res);
-        })
-    }
+    update: function(table, objColVals, condition, cb) {
+        var queryString = `UPDATE ${table}`;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    takeoff(table, condition, cb) {
+        var queryString = `DELETE FROM ${table}`;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
 };
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,18 +1,59 @@
 var express = require("express");
 
 var router = express.Router();
-
 var burger = require("../models/burger");
 
 router.get("/", function(req, res) {
     burger.all(function(data) {
-        res.render("index", { burgers: data });
+        var hbsObject = {
+            burgers: data,
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
     });
 });
 
+
 router.post("/api/burgers", function(req, res) {
-    burger.insertOne(req.body.burger_name, function(result) {
-        res.json({ id: result.insertId });
+    var devoured = 0;
+    if (req.body.devoured === "true") {
+        devoured = 1;
+    }
+    burger.create(
+        ["burger_name", "devoured"], [req.body.burger_name, devoured],
+        function(result) {
+            console.log(result);
+            res.json({ id: result.insertId });
+        }
+    );
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = `id = ${req.params.id}`;
+
+    console.log("condition", condition);
+
+    burger.update({
+            devoured: req.body.devoured,
+        }, condition,
+        function(result) {
+            if (result.changedRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+});
+
+router.takeoff("/api/burgers/:id", function(req, res) {
+    const condition = `id = ${req.params.id}`;
+
+    burger.takeoff(condition, function(result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
     });
 });
+
 module.exports = router;
